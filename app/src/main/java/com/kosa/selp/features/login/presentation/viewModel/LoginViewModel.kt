dd54959925931470126b9c959660e43e4c08ccd1
pch8349 <chan449@naver.com>
2025-07-24 17:27:15 +0900

    feat: 카카오 로그인 통신

---
+++ app/src/main/java/com/kosa/selp/features/login/presentation/viewModel/LoginViewModel.kt
@@ -0,0 +1,73 @@
+package com.kosa.selp.features.login.presentation.viewModel
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor() : ViewModel() {
+
+    private val _loginEvent = MutableSharedFlow<LoginEvent>()
+    val loginEvent = _loginEvent.asSharedFlow()
+
+    fun loginWithKakao(context: Context) {
+        viewModelScope.launch {
+            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                if (error != null) {
+                    handleLoginError(error)
+                } else if (token != null) {
+                    handleLoginSuccess(token)
+                }
+            }
+
+            // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+                UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+                    if (error != null) {
+                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+                        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                        UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+                    } else if (token != null) {
+                        handleLoginSuccess(token)
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+            }
+        }
+    }
+
+    private fun handleLoginSuccess(token: OAuthToken) {
+        Log.i("LoginViewModel", "카카오계정으로 로그인 성공, authCode: ${token.accessToken}")
+        // TODO: 백엔드에 authCode 전달 로직 추가
+        viewModelScope.launch {
+            _loginEvent.emit(LoginEvent.LoginSuccess)
+        }
+    }
+
+    private fun handleLoginError(error: Throwable) {
+        Log.e("LoginViewModel", "카카오계정으로 로그인 실패", error)
+        viewModelScope.launch {
+            _loginEvent.emit(LoginEvent.LoginFailure)
+        }
+    }
+}
+
+sealed class LoginEvent {
+    data object LoginSuccess : LoginEvent()
+    data object LoginFailure : LoginEvent()
+}
