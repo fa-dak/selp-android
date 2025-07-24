db2f872b057ea3b97ff924d27920ac90fa2024ac
pch8349 <chan449@naver.com>
2025-07-24 16:51:33 +0900

    feat: 카카오 로그인 기본 SDK 설정 및 연동

--- app/src/main/java/com/kosa/selp/SelpApplication.kt
+++ app/src/main/java/com/kosa/selp/SelpApplication.kt
@@ -1,7 +1,15 @@
 package com.kosa.selp
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SelpApplication : Application()
+class SelpApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+    }
+}
