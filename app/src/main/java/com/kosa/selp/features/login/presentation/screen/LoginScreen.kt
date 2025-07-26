339985c7e6db9362fb618763cfc803ee5ebd8ada
sangzun-han <hsjun1996@naver.com>
2025-07-27 00:25:03 +0900

    feat : 로그인 화면

--- app/src/main/java/com/kosa/selp/features/login/presentation/screen/LoginScreen.kt
+++ app/src/main/java/com/kosa/selp/features/login/presentation/screen/LoginScreen.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -17,15 +20,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kosa.selp.R
 import com.kosa.selp.shared.theme.AppColor
 
 @Composable
-fun LoginScreen(
-    onKakaoLoginClick: () -> Unit
-) {
+fun LoginScreen(onKakaoLoginClick: () -> Unit) {
+    val bottomPadding = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
+
     Box(modifier = Modifier.fillMaxSize()) {
         Image(
             painter = painterResource(id = R.drawable.selp_background),
@@ -60,15 +62,13 @@ fun LoginScreen(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
                 .fillMaxWidth()
-                .padding(horizontal = 24.dp, vertical = 24.dp)
+                .padding(
+                    start = 24.dp,
+                    end = 24.dp,
+                    bottom = bottomPadding + 24.dp
+                )
                 .height(48.dp)
                 .clickable { onKakaoLoginClick() }
         )
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun LoginScreenPreview() {
-    LoginScreen(onKakaoLoginClick = {})
 }
