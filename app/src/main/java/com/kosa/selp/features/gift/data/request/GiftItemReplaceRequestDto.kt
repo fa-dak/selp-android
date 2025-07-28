5b492e6dc91cd4879a993584a2f712ddd1477dc4
sangzun-han <hsjun1996@naver.com>
2025-07-29 00:44:19 +0900

    feat: 추천 선물 교체 API 및 유스케이스 추가

--- app/src/main/java/com/kosa/selp/features/gift/data/request/GiftItemReplaceRequestDto.kt
+++ app/src/main/java/com/kosa/selp/features/gift/data/request/GiftItemReplaceRequestDto.kt
@@ -1,2 +1,29 @@
 package com.kosa.selp.features.gift.data.request
 
+import com.google.gson.annotations.SerializedName
+
+data class GiftItemReplaceRequestDto(
+    @SerializedName("productId")
+    val productId: Long,
+
+    @SerializedName("ageRange")
+    val ageRange: String?,
+
+    @SerializedName("anniversaryType")
+    val anniversaryType: String?,
+
+    @SerializedName("category")
+    val category: String?,
+
+    @SerializedName("relation")
+    val relation: String?,
+
+    @SerializedName("gender")
+    val gender: String?,
+
+    @SerializedName("price")
+    val price: Long,
+
+    @SerializedName("userMessage")
+    val userMessage: String?
+)
