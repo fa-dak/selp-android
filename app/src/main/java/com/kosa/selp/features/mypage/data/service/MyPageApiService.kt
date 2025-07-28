eae3e474fe4ddd16ae4bd7196eb1cb19e97da3bc
pch8349 <chan449@naer.com>
2025-07-29 03:23:09 +0900

    feat : 내 주변인 세부조회 api 연결

--- app/src/main/java/com/kosa/selp/features/mypage/data/service/MyPageApiService.kt
+++ app/src/main/java/com/kosa/selp/features/mypage/data/service/MyPageApiService.kt
@@ -2,7 +2,10 @@ package com.kosa.selp.features.mypage.data.service
 
 import com.kosa.selp.features.mypage.model.Contact
 import com.kosa.selp.features.mypage.model.GiftBundleResponse
+import com.kosa.selp.features.mypage.model.ReceiverModifyRequest
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -24,4 +27,10 @@ interface MyPageApiService {
     suspend fun getMyReceiverInfoDetail(
         @Path("receiver-info-id") receiverInfoId: Long
     ): Contact
+
+    @PUT("/receiver-infos/{receiver-info-id}")
+    suspend fun modifyReceiverInfo(
+        @Path("receiver-info-id") receiverInfoId: Long,
+        @Body request: ReceiverModifyRequest
+    )
 }
