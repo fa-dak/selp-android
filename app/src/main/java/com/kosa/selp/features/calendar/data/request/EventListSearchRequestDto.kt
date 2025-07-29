934d95f9be443d878bfc4cdfb0f640a8da4b039c
sangzun-han <hsjun1996@naver.com>
2025-07-29 14:02:33 +0900

    fix : 재영이가 만든 기존 달력 관련 composable 모두 제거

--- app/src/main/java/com/kosa/selp/features/calendar/data/request/EventListSearchRequestDto.kt
+++
@@ -1,6 +0,0 @@
-package com.kosa.selp.features.calendar.data.request
-
-data class EventListSearchRequestDto(
-    val year: Int,
-    val month: Int,
-)
--- app/src/main/java/com/kosa/selp/features/calendar/dto/CalendarEventRegisterDto.kt
+++
@@ -1,9 +0,0 @@
-package com.kosa.selp.features.calendar.dto
-
-data class CalendarEventRegisterDto(
-    val eventDate: String,
-    val eventName: String,
-    val eventType: String,
-    val receiverInfoId: Long,
-    val notificationDaysBefore: Int,
-)
