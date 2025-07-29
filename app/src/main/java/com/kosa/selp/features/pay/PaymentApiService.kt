f3e6398f8484c79ed6e5cce6fa6a2978365c9885
JaeYoungJung <hanol98@naver.com>
2025-07-30 01:16:50 +0900

    fix: 상품 백엔드 결제 경로 수정

--- app/src/main/java/com/kosa/selp/features/pay/PaymentApiService.kt
+++ app/src/main/java/com/kosa/selp/features/pay/PaymentApiService.kt
@@ -3,10 +3,12 @@ package com.kosa.selp.features.pay
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface PaymentApiService {
-    @POST("/payment/verify")
+    @POST("/payments/products/{productId}")
     suspend fun verify(
+        @Path("productId") productId: Long,
         @Body
         request: PaymentVerifyRequest
     ): Response<PaymentVerifyResponse>
